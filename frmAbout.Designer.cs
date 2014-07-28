d2860cbb995648830fc422b20b2c7f469aef4a78
Philip Bailey <philip@northarrowresearch.com>
2014-07-28 09:43:19 -0700

    About from tidied and polished.

--- frmAbout.Designer.cs
+++ frmAbout.Designer.cs
@@ -29,17 +29,61 @@
         private void InitializeComponent()
         {
             this.cmdOK = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblDBVersion = new System.Windows.Forms.Label();
+            this.lblWebSite = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // cmdOK
             // 
+            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.cmdOK.Location = new System.Drawing.Point(369, 378);
+            this.cmdOK.Location = new System.Drawing.Point(523, 254);
             this.cmdOK.Name = "cmdOK";
             this.cmdOK.Size = new System.Drawing.Size(75, 23);
             this.cmdOK.TabIndex = 0;
-            this.cmdOK.Text = "button1";
+            this.cmdOK.Text = "OK";
             this.cmdOK.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CHaMPWorkbench.Properties.Resources.NA_Logo_SM;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 264);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(190, 12);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(52, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "lblVersion";
+            // 
+            // lblDBVersion
+            // 
+            this.lblDBVersion.AutoSize = true;
+            this.lblDBVersion.Location = new System.Drawing.Point(190, 44);
+            this.lblDBVersion.Name = "lblDBVersion";
+            this.lblDBVersion.Size = new System.Drawing.Size(67, 13);
+            this.lblDBVersion.TabIndex = 3;
+            this.lblDBVersion.Text = "lblDBVersion";
+            // 
+            // lblWebSite
+            // 
+            this.lblWebSite.AutoSize = true;
+            this.lblWebSite.Location = new System.Drawing.Point(190, 76);
+            this.lblWebSite.Name = "lblWebSite";
+            this.lblWebSite.Size = new System.Drawing.Size(55, 13);
+            this.lblWebSite.TabIndex = 4;
+            this.lblWebSite.TabStop = true;
+            this.lblWebSite.Text = "linkLabel1";
             // 
             // frmAbout
             // 
@@ -47,19 +91,30 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cmdOK;
-            this.ClientSize = new System.Drawing.Size(456, 413);
+            this.ClientSize = new System.Drawing.Size(610, 289);
+            this.Controls.Add(this.lblWebSite);
+            this.Controls.Add(this.lblDBVersion);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cmdOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAbout";
             this.Text = "CHaMP Workbench";
+            this.Load += new System.EventHandler(this.frmAbout_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblDBVersion;
+        private System.Windows.Forms.LinkLabel lblWebSite;
     }
 }
--- frmAbout.cs
+++ frmAbout.cs
@@ -6,14 +6,51 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace CHaMPWorkbench
 {
     public partial class frmAbout : Form
     {
-        public frmAbout()
+        private OleDbConnection m_dbCon;
+
+        public frmAbout(OleDbConnection dbCon)
         {
             InitializeComponent();
+            m_dbCon = dbCon;
         }
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text =  CHaMPWorkbench.Properties.Resources.MyApplicationNameLong + " version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            try
+            {
+                if (m_dbCon.State != ConnectionState.Open)
+                    m_dbCon.Open();
+
+                OleDbCommand dbCom = new OleDbCommand("SELECT ValueInfo FROM VersionInfo WHERE Key = 'DatabaseVersion'", m_dbCon);
+                String sVersion = (string)dbCom.ExecuteScalar();
+                if (String.IsNullOrWhiteSpace(sVersion))
+                    throw new Exception("Error retrieving database version");
+
+                lblDBVersion.Text = "Database version: " + sVersion;
+            }
+            catch (Exception ex)
+            {
+                lblDBVersion.Text = "Error retrieving database version.";
+            }
+
+            lblWebSite.Text = "Web Site: " + CHaMPWorkbench.Properties.Resources.WebSiteURL;
+            lblWebSite.LinkArea = new LinkArea(lblWebSite.Text.Length - CHaMPWorkbench.Properties.Resources.WebSiteURL.Length, CHaMPWorkbench.Properties.Resources.WebSiteURL.Length);
+            lblWebSite.Links[0].LinkData = CHaMPWorkbench.Properties.Resources.WebSiteURL;
+            lblWebSite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData as string);
+         }
     }
 }
