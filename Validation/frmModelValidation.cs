07a53e31e21711636baa84bbae36fd7e7827e93f
pipbailey <philip@northarrowresearch.com>
2016-03-10 13:03:22 -0800

    moving XSL into dist folder

--- Validation/frmModelValidation.cs
+++ Validation/frmModelValidation.cs
@@ -19,13 +19,24 @@ namespace CHaMPWorkbench.Validation
         private void frmModelValidation_Load(object sender, EventArgs e)
         {
 
+            foreach (string sReportXSLPath in System.IO.Directory.GetFiles(ReportFolder, "*.xsl", System.IO.SearchOption.TopDirectoryOnly))
+            {
+                System.Xml.XmlDocument xmlReport = new System.Xml.XmlDocument();
+                xmlReport.LoadXml(sReportXSLPath);
+
+            }
+
+            // html/head//title
         }
 
         private string ReportFolder
         {
             get
             {
-                string sReportFolder = System.Reflection.Assembly.GetExecutingAssembly().Location
+                string sReportFolder = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                sReportFolder = System.IO.Path.Combine(sReportFolder, "ReportTransforms");
+                sReportFolder = System.IO.Path.Combine(sReportFolder, "dist");
+                return sReportFolder;
             }
         }
     }
