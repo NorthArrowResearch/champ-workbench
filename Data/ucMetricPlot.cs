e51e1f6000236952d6ecb88b32779be9a334c029
pipbailey <philip@northarrowresearch.com>
2016-05-26 16:39:19 -0700

    data plotting correctly

--- Data/ucMetricPlot.cs
+++ Data/ucMetricPlot.cs
@@ -16,7 +16,7 @@ namespace CHaMPWorkbench.Data
         public string DBCon { get; set; }
         public int VisitID { get; set; }
 
-        private List<MetricValue> MetricValues { get; set; }
+        private Dictionary<int, ModelResult> m_dModelResults;
 
         public ucMetricPlot()
         {
@@ -26,7 +26,7 @@ namespace CHaMPWorkbench.Data
         private void ucMetricPlot_Load(object sender, EventArgs e)
         {
             PlotType.LoadPlotTypes(ref cboPlotTypes, DBCon);
-            ModelResult.LoadModelResults(ref cboModelResults, DBCon, VisitID);
+            ModelResult.LoadModelResults(ref cboModelResults, DBCon, VisitID, out m_dModelResults);
 
             cboModelResults.SelectedIndexChanged += PlotChanged;
             cboPlotTypes.SelectedIndexChanged += PlotChanged;
@@ -66,6 +66,60 @@ namespace CHaMPWorkbench.Data
             return dResults;
         }
 
+        private void Combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboModelResults.SelectedItem is ModelResult && cboPlotTypes.SelectedItem is PlotType)
+            {
+                // TODO replot graph
+                UpdatePlot(cboPlotTypes.SelectedItem as PlotType, cboModelResults.SelectedItem as ModelResult);
+            }
+        }
+
+        private void UpdatePlot(PlotType thePlot, ModelResult theResult)
+        {
+            chtData.Series.Clear();
+
+
+            Series watershedSeries = chtData.Series.Add(theResult.Watershed);
+            watershedSeries.ChartType = SeriesChartType.Point;
+            foreach (ModelResult aResult in m_dModelResults.Values)
+            {
+                if (aResult.MetricValues.ContainsKey(thePlot.XMetricID) && aResult.MetricValues.ContainsKey(thePlot.YMetricID))
+                    watershedSeries.Points.AddXY(aResult.MetricValues[thePlot.XMetricID], aResult.MetricValues[thePlot.YMetricID]);
+            }
+
+            if (m_dModelResults.ContainsKey(theResult.ID) && theResult.MetricValues.ContainsKey(thePlot.XMetricID) && theResult.MetricValues.ContainsKey(thePlot.YMetricID))
+            {
+                // Add the specific result for the target visit
+                Series visitSeries = chtData.Series.Add(string.Format("Visit {0}", theResult.VisitID));
+                visitSeries.ChartType = SeriesChartType.Point;
+                visitSeries.Points.AddXY(theResult.MetricValues[thePlot.XMetricID], theResult.MetricValues[thePlot.YMetricID]);
+
+                visitSeries.Color = Color.Red;
+                visitSeries.MarkerSize = 10;
+            }
+
+            ChartArea pChartArea = chtData.ChartAreas[0];
+            if (chtData.Titles.Count < 1)
+                chtData.Titles.Add("ChartTitle");
+            chtData.Titles[0].Text = thePlot.Title;
+
+            pChartArea.AxisX.Title = thePlot.XMetric;
+            pChartArea.AxisX.MajorGrid.LineColor = Color.LightGray;
+            pChartArea.AxisX.MajorTickMark.LineColor = Color.Black;
+            pChartArea.AxisX.RoundAxisValues();
+
+            pChartArea.AxisY.Title = thePlot.YMetric;
+            pChartArea.AxisY.MajorGrid.LineColor = Color.LightGray;
+            pChartArea.AxisY.MajorTickMark.LineColor = Color.Black;
+
+            //enable scroll and zoom
+            pChartArea.CursorX.IsUserEnabled = true;
+            pChartArea.CursorX.IsUserSelectionEnabled = true;
+            pChartArea.CursorY.IsUserEnabled = true;
+            pChartArea.CursorY.IsUserSelectionEnabled = true;
+        }
+
         # region HelperClasses
 
         private class PlotType
@@ -73,16 +127,21 @@ namespace CHaMPWorkbench.Data
             public int PlotID { get; internal set; }
             public string Title { get; internal set; }
             public int XMetricID { get; internal set; }
+            public string XMetric { get; internal set; }
             public int YMetricID { get; internal set; }
+            public string YMetric { get; internal set; }
             public int PlotTypeID { get; internal set; }
 
-            public PlotType(int nPlotID, string sTitle, int nXMetricID, int nYMetricID, int nPlotTypeID)
+            public PlotType(int nPlotID, string sTitle, int nXMetricID, string sXMetric, int nYMetricID, string sYMetric, int nPlotTypeID)
             {
                 PlotID = nPlotID;
                 Title = sTitle;
                 XMetricID = nXMetricID;
+                XMetric = sXMetric;
                 YMetricID = nYMetricID;
+                YMetric = sYMetric;
                 PlotTypeID = nPlotTypeID;
+
             }
 
             public override string ToString()
@@ -97,12 +156,20 @@ namespace CHaMPWorkbench.Data
                 using (OleDbConnection dbCon = new OleDbConnection(sDBCon))
                 {
                     dbCon.Open();
-                    OleDbCommand dbCom = new OleDbCommand("SELECT PlotID, PlotTitle, XMetricID, YMetricID, PlotTypeID FROM Metric_Plots ORDER BY PlotTitle", dbCon);
+                    OleDbCommand dbCom = new OleDbCommand("SELECT P.PlotID, P.PlotTitle, P.XMetricID, P.YMetricID, P.PlotTypeID, Y.Title AS YTitle, X.Title AS XTitle" +
+                        " FROM (Metric_Definitions AS X INNER JOIN Metric_Plots AS P ON X.MetricID = P.XMetricID) INNER JOIN Metric_Definitions AS Y ON P.YMetricID = Y.MetricID ORDER BY P.PlotTitle", dbCon);
+
                     OleDbDataReader dbRead = dbCom.ExecuteReader();
                     while (dbRead.Read())
                     {
-                        cbo.Items.Add(new PlotType(dbRead.GetInt32(dbRead.GetOrdinal("PlotID")), dbRead.GetString(dbRead.GetOrdinal("PlotTitle")),
-                            dbRead.GetInt32(dbRead.GetOrdinal("XMetricID")), dbRead.GetInt32(dbRead.GetOrdinal("YMetricID")), dbRead.GetInt32(dbRead.GetOrdinal("PlotTypeID"))));
+                        cbo.Items.Add(new PlotType(
+                            dbRead.GetInt32(dbRead.GetOrdinal("PlotID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("PlotTitle"))
+                            , dbRead.GetInt32(dbRead.GetOrdinal("XMetricID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("XTitle"))
+                            , dbRead.GetInt32(dbRead.GetOrdinal("YMetricID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("YTitle"))
+                            , dbRead.GetInt32(dbRead.GetOrdinal("PlotTypeID"))));
                     }
 
                     if (cbo.Items.Count > 0)
@@ -177,6 +244,13 @@ namespace CHaMPWorkbench.Data
             public string Site { get; internal set; }
             public int VisitID { get; internal set; }
 
+            public Dictionary<int, double> MetricValues { get; internal set; }
+
+            public void SetMetricValue(int nMetricID, double fValue)
+            {
+                MetricValues[nMetricID] = fValue;
+            }
+
             public string Title
             {
                 get
@@ -218,48 +292,73 @@ namespace CHaMPWorkbench.Data
                 SiteID = nSiteID;
                 Site = sSiteName;
                 VisitID = nVisitID;
+
+                MetricValues = new Dictionary<int, double>();
             }
 
-            public static void LoadModelResults(ref ComboBox cbo, string sDBCon, int VisitID)
+            public static void LoadModelResults(ref ComboBox cbo, string sDBCon, int VisitID, out Dictionary<int, ModelResult> dModelResults)
             {
                 cbo.Items.Clear();
+                dModelResults = new Dictionary<int, ModelResult>();
 
-                using (OleDbConnection dbCon = new OleDbConnection(sDBCon))
+                using (OleDbConnection conResults = new OleDbConnection(sDBCon))
                 {
-                    dbCon.Open();
-                    OleDbCommand dbCom = new OleDbCommand("SELECT R.ResultID, R.ModelVersion, R.RunDateTime, L.Title AS ScavengeType, R.[VisitID], S.SiteName, S.SiteID, W.WatershedID, W.WatershedName" +
-                        " FROM CHAMP_Watersheds AS W INNER JOIN ((LookupListItems AS L INNER JOIN Metric_Results AS R ON L.ItemID = R.ScavengeTypeID) INNER JOIN (CHAMP_Sites AS S INNER JOIN CHAMP_Visits AS V ON S.SiteID = V.SiteID) ON R.VisitID = V.VisitID) ON W.WatershedID = S.WatershedID" +
-                        " WHERE (R.VisitID = @VisitID) ORDER BY R.RunDateTime DESC", dbCon);
-                    dbCom.Parameters.AddWithValue("@VisitID", VisitID);
-                    OleDbDataReader dbRead = dbCom.ExecuteReader();
-                    while (dbRead.Read())
-                    {
-                        ModelResult theResult = null;
-                        if (dbRead.IsDBNull(dbRead.GetOrdinal("RunDateTime")))
-                            theResult = new ModelResult(
-                                dbRead.GetInt32(dbRead.GetOrdinal("ResultID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("ModelVersion")) 
-                                , dbRead.GetString(dbRead.GetOrdinal("ScavengeType"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("WatershedID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("WatershedName"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("SiteID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("SiteName"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("VisitID"))   
-                                );
-                        else
-                            theResult = new ModelResult(
-                                dbRead.GetInt32(dbRead.GetOrdinal("ResultID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("ModelVersion"))
-                                , dbRead.GetDateTime(dbRead.GetOrdinal("RunDateTime"))
-                                , dbRead.GetString(dbRead.GetOrdinal("ScavengeType"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("WatershedID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("WatershedName"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("SiteID"))
-                                , dbRead.GetString(dbRead.GetOrdinal("SiteName"))
-                                , dbRead.GetInt32(dbRead.GetOrdinal("VisitID"))                                
-                                );
+                    conResults.Open();
+                    OleDbCommand comResults = new OleDbCommand("SELECT R.ResultID, R.ModelVersion, R.RunDateTime, L.Title AS ScavengeType, R.VisitID, S.SiteName, S.SiteID, W.WatershedID, W.WatershedName" +
+" FROM LookupListItems AS L INNER JOIN (CHAMP_Watersheds AS W INNER JOIN (CHAMP_Sites AS S INNER JOIN (Metric_Results AS R INNER JOIN CHAMP_Visits AS V ON R.VisitID = V.VisitID) ON S.SiteID = V.SiteID) ON W.WatershedID = S.WatershedID) ON L.ItemID = R.ScavengeTypeID" +
+" WHERE (((W.WatershedID) In (SELECT SS.WatershedID FROM CHAMP_Sites AS SS INNER JOIN CHAMP_Visits AS VV ON SS.SiteID = VV.SiteID WHERE (((VV.VisitID)=@VisitID)))))" +
+" ORDER BY R.RunDateTime DESC", conResults);
+                    comResults.Parameters.AddWithValue("@VisitID", VisitID);
 
-                        cbo.Items.Add(theResult);
+                    using (OleDbConnection conMetricValues = new OleDbConnection(sDBCon))
+                    {
+                        conMetricValues.Open();
+                        OleDbCommand comMetricValues = new OleDbCommand("SELECT MetricID, MetricValue FROM Metric_VisitMetrics WHERE (ResultID = @ResultID) AND (MetricValue IS NOT NULL)", conMetricValues);
+                        OleDbParameter pResultID = comMetricValues.Parameters.Add("@ResultID", OleDbType.Integer);
+
+
+                        OleDbDataReader rdResults = comResults.ExecuteReader();
+                        while (rdResults.Read())
+                        {
+                            int nResultID = rdResults.GetInt32(rdResults.GetOrdinal("ResultID"));
+
+                            ModelResult theResult = null;
+                            if (rdResults.IsDBNull(rdResults.GetOrdinal("RunDateTime")))
+                                theResult = new ModelResult(nResultID
+                                    , rdResults.GetString(rdResults.GetOrdinal("ModelVersion"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("ScavengeType"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("WatershedID"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("WatershedName"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("SiteID"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("SiteName"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("VisitID"))
+
+
+                                    );
+                            else
+                                theResult = new ModelResult(nResultID
+                                    , rdResults.GetString(rdResults.GetOrdinal("ModelVersion"))
+                                    , rdResults.GetDateTime(rdResults.GetOrdinal("RunDateTime"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("ScavengeType"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("WatershedID"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("WatershedName"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("SiteID"))
+                                    , rdResults.GetString(rdResults.GetOrdinal("SiteName"))
+                                    , rdResults.GetInt32(rdResults.GetOrdinal("VisitID"))
+                                    );
+
+                            // Now add the metric values to this result
+                            pResultID.Value = nResultID;
+                            OleDbDataReader rdMetricValues = comMetricValues.ExecuteReader();
+                            while (rdMetricValues.Read())
+                                theResult.SetMetricValue(rdMetricValues.GetInt32(rdMetricValues.GetOrdinal("MetricID")), rdMetricValues.GetDouble(rdMetricValues.GetOrdinal("MetricValue")));
+                            rdMetricValues.Close();
+
+                            dModelResults.Add(theResult.ID, theResult);
+
+                            if (theResult.VisitID == VisitID)
+                                cbo.Items.Add(theResult);
+                        }
                     }
 
                     if (cbo.Items.Count > 0)
@@ -269,28 +368,5 @@ namespace CHaMPWorkbench.Data
         }
 
         #endregion
-
-        private void Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (cboModelResults.SelectedItem is ModelResult && cboPlotTypes.SelectedItem is PlotType)
-            {
-                MetricValues = MetricValue.LoadMetricValues(DBCon, cboPlotTypes.SelectedItem as PlotType, cboModelResults.SelectedItem as ModelResult);
-                // TODO replot graph
-                UpdatePlot();
-            }
-        }
-
-        private void UpdatePlot()
-        {
-            chtData.Series.Clear();
-
-            if (MetricValues == null || MetricValues.Count < 1)
-                return;
-
-            Series theSeries = chtData.Series.Add("test");
-
-            foreach (MetricValue aValue in MetricValues)
-                theSeries.Points.Add(new DataPoint(aValue.Value, aValue.Value));
-        }
     }
 }
