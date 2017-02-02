0774b352da3661e6f006c56f06e68e7ee183f0bf
Philip Bailey <philip@northarrowresearch.com>
2017-02-01 16:12:37 -0800

    new all CHaMP data class

---
+++ CHaMPData/AllCHaMPData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHaMPWorkbench.CHaMPData
+{
+    class AllCHaMPData
+    {
+        public Dictionary<long, Watershed> Watersheds { get; internal set; }
+        public Dictionary<long, Site> Sites { get; internal set; }
+        public Dictionary<long, Visit> Visits { get; internal set; }
+
+        public AllCHaMPData(string sDBCon)
+        {
+            Watersheds = Watershed.Load(sDBCon);
+            Sites = Site.Load(sDBCon);
+            Visits = Visit.Load(sDBCon);
+        }
+    }
+}
