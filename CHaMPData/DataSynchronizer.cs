97ee0bd4a785eaccc254714b7d1003137d9648e1
Philip Bailey <philip@northarrowresearch.com>
2017-02-20 12:31:37 -0800

    just calling site summary API not details

--- CHaMPData/DataSynchronizer.cs
+++ CHaMPData/DataSynchronizer.cs
@@ -79,33 +79,20 @@ namespace CHaMPWorkbench.CHaMPData
             ApiResponse<GeoOptix.API.Model.SiteSummaryModel[]> response = api.Get<GeoOptix.API.Model.SiteSummaryModel[]>();
             foreach (GeoOptix.API.Model.SiteSummaryModel apiSite in response.Payload)
             {
-                ApiHelper api2 = new ApiHelper(apiSite.Url, api.AuthToken);
-                ApiResponse<GeoOptix.API.Model.SiteModel> apiSiteDetailsresp = api2.Get<GeoOptix.API.Model.SiteModel>();
-                GeoOptix.API.Model.SiteModel apisiteDetails = apiSiteDetailsresp.Payload;
-
-                if (apisiteDetails != null && dWatershedURLs.ContainsKey(apisiteDetails.WatershedUrl))
+                if (dWatershedURLs.ContainsKey(apiSite.WatershedUrl)) // && apisiteDetails != null)
                 {
-                    long nWatershedID = dWatershedURLs[apisiteDetails.WatershedUrl];
-
-                    Nullable<double> fLatitude = new Nullable<double>();
-                    if (!string.IsNullOrEmpty(apisiteDetails.Latitude))
-                        fLatitude = double.Parse(apisiteDetails.Latitude);
-
-                    Nullable<double> fLongitude = new Nullable<double>();
-                    if (!string.IsNullOrEmpty(apisiteDetails.Longitude))
-                        fLongitude = double.Parse(apisiteDetails.Longitude);
-
+                    long nWatershedID = dWatershedURLs[apiSite.WatershedUrl];
 
                     if (dSites.ContainsKey((long)apiSite.Id))
                     {
-                        dSites[(long)apiSite.Id].Name = apisiteDetails.Name;
-                        //dSites[(long)apiSite.Id].UTMZone = string.Empty;
-                        dSites[(long)apiSite.Id].Latitude = fLatitude;
-                        dSites[(long)apiSite.Id].Longitude = fLongitude;
+                        dSites[(long)apiSite.Id].Name = apiSite.Name;
                     }
                     else
                     {
-                        dSites[(long)apiSite.Id] = new CHaMPData.Site(apiSite.Id, apisiteDetails.Name, nWatershedID, string.Empty, string.Empty, string.Empty, false, false, false, false, false, false, fLatitude, fLongitude, null, naru.db.DBState.New);
+                        Nullable<double> fLongitude = new Nullable<double>();
+                        Nullable<double> fLatitude = new Nullable<double>();
+
+                        dSites[(long)apiSite.Id] = new CHaMPData.Site(apiSite.Id, apiSite.Name, nWatershedID, string.Empty, string.Empty, string.Empty, false, false, false, false, false, false, fLatitude, fLongitude, null, naru.db.DBState.New);
                     }
 
                     dSiteURLs[apiSite.Url] = apiSite.Id;
