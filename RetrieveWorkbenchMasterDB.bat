490fb8d34a19a68543b67346679353a57d0a3964
Philip Bailey <philip@northarrowresearch.com>
2017-03-08 12:09:20 -0800

    Master Database version 37

--- RetrieveWorkbenchMasterDB.bat
+++ RetrieveWorkbenchMasterDB.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET VERSION=35
+SET VERSION=37
 
 rem ***********************************************************************
 rem In order for this to work you must have installed and configured
