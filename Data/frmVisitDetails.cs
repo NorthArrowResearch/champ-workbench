6186f6bd476dc3f6f278ecd4fbb0010230ca1325
Philip Bailey <philip@northarrowresearch.com>
2017-04-24 14:18:35 -0700

    Fixing visit details when there are no channel units

--- Data/frmVisitDetails.cs
+++ Data/frmVisitDetails.cs
@@ -166,8 +166,11 @@ namespace CHaMPWorkbench.Data
             {
                 dbCon.Open();
 
-                string sSQL = "SELECT CHAMP_Watersheds.WatershedName, S.SiteName, V.*, S.UTMZone, S.UC_Chin, S.SN_Chin, S.LC_Steel, S.MC_Steel, S.UC_Steel, S.SN_Steel, S.Latitude, S.Longitude, S.GageID, P.Title AS Protocol" +
-                    " FROM (CHAMP_Watersheds INNER JOIN ((CHAMP_Sites AS S INNER JOIN CHAMP_Visits AS V ON S.SiteID = V.SiteID) INNER JOIN CHAMP_ChannelUnits ON V.VisitID = CHAMP_ChannelUnits.VisitID) ON CHAMP_Watersheds.WatershedID = S.WatershedID) INNER JOIN LookupListItems AS P ON V.ProtocolID = P.ItemID" +
+                string sSQL = "SELECT W.WatershedName, S.SiteName, V.*, S.UTMZone, S.UC_Chin, S.SN_Chin, S.LC_Steel, S.MC_Steel, S.UC_Steel, S.SN_Steel, S.Latitude, S.Longitude, S.GageID, P.Title AS Protocol" +
+                    " FROM CHaMP_Watersheds W" +
+                    " INNER JOIN CHaMP_Sites S ON W.WatershedID = S.WatershedID" +
+                    " INNER JOIN CHaMP_Visits V ON S.SiteID = V.SiteID" +
+                    " INNER JOIN LookupListItems P ON V.ProtocolID = P.ItemID" +
                     " WHERE V.VisitID = @VisitID";
 
                 SQLiteDataAdapter da = new SQLiteDataAdapter(sSQL, dbCon);
