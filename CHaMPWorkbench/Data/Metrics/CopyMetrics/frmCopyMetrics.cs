8ba2d4d6888bb153c557b140e0956564963d2abe
Philip Bailey <philip@northarrowresearch.com>
2017-09-19 14:57:40 -0700

    Testing and fixing copy metrics

--- CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.cs
+++ CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.cs
@@ -55,7 +55,7 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                     naru.db.sqlite.SQLiteHelpers.AddStringParameterN(ref dbCom, txtRemarks.Text, "Remarks");
 
                     dbCom.Parameters.AddWithValue("SchemaID", ((naru.db.NamedObject)cboDestination.SelectedItem).ID);
-                    dbCom.Parameters.AddWithValue("ScavengeTypeID", 1);
+                    dbCom.Parameters.AddWithValue("ScavengeTypeID", 10022);
                     dbCom.ExecuteNonQuery();
 
                     dbCom = new SQLiteCommand("SELECT last_insert_rowid()", dbTrans.Connection, dbTrans);
@@ -72,15 +72,17 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                     string sqlInsert = string.Empty;
                     switch (sourceBatch.DatabaseTable.ToLower())
                     {
-                        case "metric_visitmetrics":
-                            sqlInsert = "SELECT T.InstanceID, M.MetricID, M.MetricValue" +
-                                " FROM Metric_VisitMetrics M" +
-                                " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
-                                " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
-                                " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
-                            break;
+                        MetricBatch batch = (MetricBatch)aRow.DataBoundItem;
+                        if (!batch.Copy)
+                            continue;
 
-                        case "metric_tiermetrics":
+                        // Insert the metric instances
+                        dbCom = new SQLiteCommand("INSERT INTO Metric_Instances (BatchID, VisitID, ModelVersion, MetricsCalculatedOn, APIInsertionOn, WorkbenchInsertionOn)" +
+                        "SELECT @DestBatchID, VisitID, ModelVersion, MetricsCalculatedOn, APIInsertionOn, WorkbenchInsertionOn FROM Metric_Instances WHERE (BatchID = @SourceBatchID)" +
+                        " AND ( VisitID NOT IN (SELECT VisitID FROM Metric_Instances WHERE BatchID = @DestBatchID) )", dbCon, dbTrans);
+                        dbCom.Parameters.AddWithValue("SourceBatchID", batch.ID);
+                        dbCom.Parameters.AddWithValue("DestBatchID", nDestinationBatchID);
+                        dbCom.ExecuteNonQuery();
 
                             sqlInsert = "SELECT T.InstanceID, M.MetricID, M.TierID, M.MetricValue" +
                               " FROM Metric_TierMetrics M" +
@@ -97,8 +99,30 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                                 " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
                             break;
 
-                        default:
-                            throw new Exception("Unhandled database table");
+                                sqlInsert = "SELECT T.InstanceID, M.MetricID, M.TierID, M.MetricValue" +
+                                  " FROM Metric_TierMetrics M" +
+                                  " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
+                                  " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
+                                  " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
+                                break;
+
+                            case "metric_channelunitmetrics":
+                                sqlInsert = "SELECT T.InstanceID, M.MetricID, M.channelUnitNumber, M.MetricValue" +
+                                    " FROM Metric_ChannelUnitMetrics M" +
+                                    " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
+                                    " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
+                                    " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
+                                break;
+
+                            default:
+                                throw new Exception("Unhandled database table");
+                        }
+
+                        dbCom = new SQLiteCommand(sqlInsert, dbCon, dbTrans);
+                        dbCom.Parameters.AddWithValue("SourceBatchID", batch.ID);
+                        dbCom.Parameters.AddWithValue("DestinationBatchID", nDestinationBatchID);
+                        dbCom.Parameters.AddWithValue("DestinationSchemaID", ((naru.db.NamedObject)cboDestination.SelectedItem).ID);
+                        dbCom.ExecuteNonQuery();
                     }
 
                     dbCom = new SQLiteCommand(sqlInsert, dbCon, dbTrans);
@@ -126,9 +150,33 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                 return false;
             }
 
-            //naru.db.NamedObject batch = (naru.db.NamedObject)cboSource.SelectedItem;
-            //long nSourceSchemaID = naru.db.sqlite.SQLiteHelpers.GetScalarID(naru.db.sqlite.DBCon.ConnectionString,
-            //    string.Format("SELECT SchemaID FROM Metric_Batches WHERE BatchID = {0}", batch.ID));
+            string sourceDBTable = string.Empty;
+            foreach (DataGridViewRow aRow in grdInfo.Rows)
+            {
+                MetricBatch batch = (MetricBatch)aRow.DataBoundItem;
+                if (batch.Copy)
+                {
+                    if (string.IsNullOrEmpty(sourceDBTable))
+                        sourceDBTable = batch.DatabaseTable;
+                    else if (string.Compare(sourceDBTable, batch.DatabaseTable, true) != 0)
+                    {
+                        MessageBox.Show("You can only select multiple metric schemas that have the same dimensionality. i.e. either all Visit level metric schemas" +
+                            " or all tier 1 etc.", "Invalid Source Metric Schemas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        grdInfo.Select();
+                        return false;
+                    }
+
+                    // Check that none of the source metric schemas are selected as the destination schema
+                    if (cboDestination.SelectedItem is CHaMPData.MetricSchema)
+                    {
+                        if (batch.Schema.ID == ((CHaMPData.MetricSchema)cboDestination.SelectedItem).ID)
+                        {
+                            MessageBox.Show("You cannot select one of the source metric schemas as the destination metric schema.", "Invalid Metric Schemas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return false;
+                        }
+                    }
+                }
+            }
 
             //naru.db.NamedObject destSchema = (naru.db.NamedObject)cboDestination.SelectedItem;
 
