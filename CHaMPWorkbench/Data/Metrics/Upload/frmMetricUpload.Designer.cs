13c1f816e29b33c1777010346ab71bdcf9c36a55
Philip Bailey <philip@northarrowresearch.com>
2017-09-19 15:04:46 -0700

    Integrating metric uploader UI from stash

--- CHaMPWorkbench/Data/Metrics/Upload/frmMetricUpload.Designer.cs
+++ CHaMPWorkbench/Data/Metrics/Upload/frmMetricUpload.Designer.cs
@@ -28,61 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMetricUpload));
-            this.cmdCancel = new System.Windows.Forms.Button();
-            this.cmdOK = new System.Windows.Forms.Button();
+            this.cboCancel = new System.Windows.Forms.Button();
+            this.cboOK = new System.Windows.Forms.Button();
             this.cmdHelp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboBatch = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // cmdCancel
+            // cboCancel
             // 
-            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmdCancel.Location = new System.Drawing.Point(590, 383);
-            this.cmdCancel.Name = "cmdCancel";
-            this.cmdCancel.Size = new System.Drawing.Size(75, 23);
-            this.cmdCancel.TabIndex = 0;
-            this.cmdCancel.Text = "Cancel";
-            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cboCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cboCancel.Location = new System.Drawing.Point(494, 202);
+            this.cboCancel.Name = "cboCancel";
+            this.cboCancel.Size = new System.Drawing.Size(75, 23);
+            this.cboCancel.TabIndex = 0;
+            this.cboCancel.Text = "Cancel";
+            this.cboCancel.UseVisualStyleBackColor = true;
             // 
-            // cmdOK
+            // cboOK
             // 
-            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmdOK.Location = new System.Drawing.Point(509, 383);
-            this.cmdOK.Name = "cmdOK";
-            this.cmdOK.Size = new System.Drawing.Size(75, 23);
-            this.cmdOK.TabIndex = 1;
-            this.cmdOK.Text = "Run";
-            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cboOK.Location = new System.Drawing.Point(413, 202);
+            this.cboOK.Name = "cboOK";
+            this.cboOK.Size = new System.Drawing.Size(75, 23);
+            this.cboOK.TabIndex = 1;
+            this.cboOK.Text = "Run";
+            this.cboOK.UseVisualStyleBackColor = true;
             // 
             // cmdHelp
             // 
-            this.cmdHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.cmdHelp.Location = new System.Drawing.Point(12, 383);
+            this.cmdHelp.Location = new System.Drawing.Point(12, 202);
             this.cmdHelp.Name = "cmdHelp";
             this.cmdHelp.Size = new System.Drawing.Size(75, 23);
             this.cmdHelp.TabIndex = 2;
             this.cmdHelp.Text = "Help";
             this.cmdHelp.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // cboBatch
+            // 
+            this.cboBatch.FormattingEnabled = true;
+            this.cboBatch.Location = new System.Drawing.Point(95, 10);
+            this.cboBatch.Name = "cboBatch";
+            this.cboBatch.Size = new System.Drawing.Size(354, 21);
+            this.cboBatch.TabIndex = 4;
+            // 
             // frmMetricUpload
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 418);
+            this.ClientSize = new System.Drawing.Size(581, 237);
+            this.Controls.Add(this.cboBatch);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdHelp);
-            this.Controls.Add(this.cmdOK);
-            this.Controls.Add(this.cmdCancel);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Controls.Add(this.cboOK);
+            this.Controls.Add(this.cboCancel);
             this.Name = "frmMetricUpload";
             this.Text = "Metric Upload";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button cmdCancel;
-        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cboCancel;
+        private System.Windows.Forms.Button cboOK;
         private System.Windows.Forms.Button cmdHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboBatch;
     }
 }
