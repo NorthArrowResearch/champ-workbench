8e3dcc1bea036b49de8b705944b0480198632d97
Philip Bailey <philip@northarrowresearch.com>
2017-09-26 11:54:40 -0700

    Workaround to copy metrics rebase issue

--- CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.Designer.cs
+++ CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.Designer.cs
@@ -31,6 +31,8 @@
             this.cmdCancel = new System.Windows.Forms.Button();
             this.cmdOK = new System.Windows.Forms.Button();
             this.cmdHelp = new System.Windows.Forms.Button();
+            this.grpSource = new System.Windows.Forms.GroupBox();
+            this.grdInfo = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtRemarks = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -38,7 +40,15 @@
             this.cboDestination = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtTitle = new System.Windows.Forms.TextBox();
-            this.ucBatch = new CHaMPWorkbench.Data.Metrics.ucBatchPicker();
+            this.colCopy = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.colProgram = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colScavengeType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSchema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVisits = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cboProgram = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpSource.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdInfo)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +56,7 @@
             // 
             this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cmdCancel.Location = new System.Drawing.Point(357, 373);
+            this.cmdCancel.Location = new System.Drawing.Point(633, 326);
             this.cmdCancel.Name = "cmdCancel";
             this.cmdCancel.Size = new System.Drawing.Size(75, 23);
             this.cmdCancel.TabIndex = 3;
@@ -57,7 +67,7 @@
             // 
             this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.cmdOK.Location = new System.Drawing.Point(276, 373);
+            this.cmdOK.Location = new System.Drawing.Point(552, 326);
             this.cmdOK.Name = "cmdOK";
             this.cmdOK.Size = new System.Drawing.Size(75, 23);
             this.cmdOK.TabIndex = 2;
@@ -68,7 +78,7 @@
             // cmdHelp
             // 
             this.cmdHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.cmdHelp.Location = new System.Drawing.Point(12, 373);
+            this.cmdHelp.Location = new System.Drawing.Point(12, 326);
             this.cmdHelp.Name = "cmdHelp";
             this.cmdHelp.Size = new System.Drawing.Size(75, 23);
             this.cmdHelp.TabIndex = 4;
@@ -76,9 +86,39 @@
             this.cmdHelp.UseVisualStyleBackColor = true;
             this.cmdHelp.Click += new System.EventHandler(this.cmdHelp_Click);
             // 
+            // grpSource
+            // 
+            this.grpSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpSource.Controls.Add(this.grdInfo);
+            this.grpSource.Location = new System.Drawing.Point(12, 42);
+            this.grpSource.Name = "grpSource";
+            this.grpSource.Size = new System.Drawing.Size(696, 133);
+            this.grpSource.TabIndex = 0;
+            this.grpSource.TabStop = false;
+            this.grpSource.Text = "Source Metrics";
+            // 
+            // grdInfo
+            // 
+            this.grdInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCopy,
+            this.colProgram,
+            this.colScavengeType,
+            this.colSchema,
+            this.colVisits});
+            this.grdInfo.Location = new System.Drawing.Point(6, 19);
+            this.grdInfo.Name = "grdInfo";
+            this.grdInfo.Size = new System.Drawing.Size(684, 108);
+            this.grdInfo.TabIndex = 3;
+            // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.txtRemarks);
             this.groupBox1.Controls.Add(this.label5);
@@ -86,9 +126,9 @@
             this.groupBox1.Controls.Add(this.cboDestination);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.txtTitle);
-            this.groupBox1.Location = new System.Drawing.Point(12, 190);
+            this.groupBox1.Location = new System.Drawing.Point(12, 181);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(420, 172);
+            this.groupBox1.Size = new System.Drawing.Size(696, 136);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Destination";
@@ -101,7 +141,7 @@
             this.txtRemarks.Location = new System.Drawing.Point(91, 84);
             this.txtRemarks.Multiline = true;
             this.txtRemarks.Name = "txtRemarks";
-            this.txtRemarks.Size = new System.Drawing.Size(323, 82);
+            this.txtRemarks.Size = new System.Drawing.Size(599, 46);
             this.txtRemarks.TabIndex = 5;
             // 
             // label5
@@ -147,15 +187,61 @@
             this.txtTitle.Size = new System.Drawing.Size(323, 20);
             this.txtTitle.TabIndex = 3;
             // 
-            // ucBatch
+            // colCopy
             // 
-            this.ucBatch.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ucBatch.Location = new System.Drawing.Point(12, 12);
-            this.ucBatch.Name = "ucBatch";
-            this.ucBatch.Size = new System.Drawing.Size(696, 163);
-            this.ucBatch.TabIndex = 5;
+            this.colCopy.DataPropertyName = "Copy";
+            this.colCopy.HeaderText = "Copy";
+            this.colCopy.Name = "colCopy";
+            this.colCopy.Width = 40;
+            // 
+            // colProgram
+            // 
+            this.colProgram.DataPropertyName = "Program";
+            this.colProgram.HeaderText = "Program";
+            this.colProgram.Name = "colProgram";
+            this.colProgram.ReadOnly = true;
+            // 
+            // colScavengeType
+            // 
+            this.colScavengeType.DataPropertyName = "ScavengeType";
+            this.colScavengeType.HeaderText = "Scavenge Type";
+            this.colScavengeType.Name = "colScavengeType";
+            this.colScavengeType.ReadOnly = true;
+            this.colScavengeType.Width = 200;
+            // 
+            // colSchema
+            // 
+            this.colSchema.DataPropertyName = "Schema";
+            this.colSchema.HeaderText = "Schema";
+            this.colSchema.Name = "colSchema";
+            this.colSchema.ReadOnly = true;
+            this.colSchema.Width = 200;
+            // 
+            // colVisits
+            // 
+            this.colVisits.DataPropertyName = "Visits";
+            this.colVisits.HeaderText = "Visits";
+            this.colVisits.Name = "colVisits";
+            this.colVisits.ReadOnly = true;
+            // 
+            // cboProgram
+            // 
+            this.cboProgram.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboProgram.FormattingEnabled = true;
+            this.cboProgram.Location = new System.Drawing.Point(83, 13);
+            this.cboProgram.Name = "cboProgram";
+            this.cboProgram.Size = new System.Drawing.Size(298, 21);
+            this.cboProgram.TabIndex = 5;
+            this.cboProgram.SelectedIndexChanged += new System.EventHandler(this.cboProgram_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Program";
             // 
             // frmCopyMetrics
             // 
@@ -164,17 +250,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cmdCancel;
             this.ClientSize = new System.Drawing.Size(720, 361);
-            this.Controls.Add(this.ucBatch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboProgram);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpSource);
             this.Controls.Add(this.cmdHelp);
             this.Controls.Add(this.cmdOK);
             this.Controls.Add(this.cmdCancel);
+            this.MinimumSize = new System.Drawing.Size(465, 361);
             this.Name = "frmCopyMetrics";
             this.Text = "Copy Metrics";
             this.Load += new System.EventHandler(this.frmCopyMetrics_Load);
+            this.grpSource.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdInfo)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -183,6 +275,8 @@
         private System.Windows.Forms.Button cmdCancel;
         private System.Windows.Forms.Button cmdOK;
         private System.Windows.Forms.Button cmdHelp;
+        private System.Windows.Forms.GroupBox grpSource;
+        private System.Windows.Forms.DataGridView grdInfo;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox txtRemarks;
         private System.Windows.Forms.Label label5;
@@ -190,6 +284,12 @@
         private System.Windows.Forms.ComboBox cboDestination;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtTitle;
-        private ucBatchPicker ucBatch;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colCopy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProgram;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colScavengeType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSchema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVisits;
+        private System.Windows.Forms.ComboBox cboProgram;
+        private System.Windows.Forms.Label label1;
     }
 }
--- CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.cs
+++ CHaMPWorkbench/Data/Metrics/CopyMetrics/frmCopyMetrics.cs
@@ -13,6 +13,7 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
 {
     public partial class frmCopyMetrics : Form
     {
+        naru.ui.SortableBindingList<MetricBatch> MetricBatches { get; set; }
         naru.ui.SortableBindingList<CHaMPData.MetricSchema> MetricSchemas { get; set; }
 
         public frmCopyMetrics()
@@ -22,11 +23,22 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
 
         private void frmCopyMetrics_Load(object sender, EventArgs e)
         {
+            grdInfo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grdInfo.AutoGenerateColumns = false;
+            grdInfo.AllowUserToAddRows = false;
+            grdInfo.AllowUserToDeleteRows = false;
+            grdInfo.AllowUserToResizeRows = false;
+            grdInfo.RowHeadersVisible = false;
+
+            MetricBatches = MetricBatch.Load();
             MetricSchemas = new naru.ui.SortableBindingList<CHaMPData.MetricSchema>(CHaMPData.MetricSchema.Load(naru.db.sqlite.DBCon.ConnectionString).Values.ToList<CHaMPData.MetricSchema>());
+
             cboDestination.DisplayMember = "Name";
             cboDestination.ValueMember = "ID";
 
-            ucBatch.ProgramChanged += cboProgram_SelectedIndexChanged;
+            cboProgram.DataSource = CHaMPData.Program.Load(naru.db.sqlite.DBCon.ConnectionString).Values.ToList<CHaMPData.Program>();
+            cboProgram.DisplayMember = "Name";
+            cboProgram.ValueMember = "ID";
         }
 
         private void cmdOK_Click(object sender, EventArgs e)
@@ -37,7 +49,6 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                 return;
             }
 
-
             using (SQLiteConnection dbCon = new SQLiteConnection(naru.db.sqlite.DBCon.ConnectionString))
             {
                 dbCon.Open();
@@ -45,8 +56,6 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
 
                 try
                 {
-                    MetricBatch sourceBatch = (MetricBatch)cboSource.SelectedItem;
-
                     // Insert the metric batch first
                     SQLiteCommand dbCom = new SQLiteCommand("INSERT INTO Metric_Batches (Title, Remarks, SchemaID, ScavengeTypeID) VALUES (@Title, @Remarks, @SchemaID, @ScavengeTypeID)", dbTrans.Connection, dbTrans);
                     naru.db.sqlite.SQLiteHelpers.AddStringParameterN(ref dbCom, txtTitle.Text, "Title");
@@ -59,8 +68,13 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                     dbCom = new SQLiteCommand("SELECT last_insert_rowid()", dbTrans.Connection, dbTrans);
                     long nDestinationBatchID = naru.db.sqlite.SQLiteHelpers.GetScalarID(ref dbCom);
 
-                    foreach (CHaMPData.MetricBatch batch in ucBatch.SelectedBatches.Values)
+
+                    foreach (DataGridViewRow aRow in grdInfo.Rows)
                     {
+                        MetricBatch batch = (MetricBatch)aRow.DataBoundItem;
+                        if (!batch.Copy)
+                            continue;
+
                         // Insert the metric instances
                         dbCom = new SQLiteCommand("INSERT INTO Metric_Instances (BatchID, VisitID, ModelVersion, MetricsCalculatedOn, APIInsertionOn, WorkbenchInsertionOn)" +
                         "SELECT @DestBatchID, VisitID, ModelVersion, MetricsCalculatedOn, APIInsertionOn, WorkbenchInsertionOn FROM Metric_Instances WHERE (BatchID = @SourceBatchID)" +
@@ -69,20 +83,19 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                         dbCom.Parameters.AddWithValue("DestBatchID", nDestinationBatchID);
                         dbCom.ExecuteNonQuery();
 
-                            sqlInsert = "SELECT T.InstanceID, M.MetricID, M.TierID, M.MetricValue" +
-                              " FROM Metric_TierMetrics M" +
-                              " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
-                              " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
-                              " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
-                            break;
+                        // insert the metric values
+                        string sqlInsert = string.Empty;
+                        switch (batch.DatabaseTable.ToLower())
+                        {
+                            case "metric_visitmetrics":
+                                sqlInsert = "SELECT T.InstanceID, M.MetricID, M.MetricValue" +
+                                    " FROM Metric_VisitMetrics M" +
+                                    " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
+                                    " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
+                                    " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
+                                break;
 
-                        case "metric_channelunitmetricS":
-                            sqlInsert = "SELECT T.InstanceID, M.MetricID, M.channelUnitNumber, M.MetricValue" +
-                                " FROM Metric_ChannelUnitMetrics M" +
-                                " INNER JOIN Metric_Instances I ON M.InstanceID = I.InstanceID" +
-                                " INNER JOIN Metric_Instances T ON I.VisitID = T.VisitID" +
-                                " WHERE (I.BatchID = @SourceBatchID) AND (T.BatchID = @DestinationBatchID) AND M.MetricID IN (SELECT MetricID FROM Metric_Schema_Definitions WHERE SchemaID = @DestinationSchemaID)";
-                            break;
+                            case "metric_tiermetrics":
 
                                 sqlInsert = "SELECT T.InstanceID, M.MetricID, M.TierID, M.MetricValue" +
                                   " FROM Metric_TierMetrics M" +
@@ -110,13 +123,8 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
                         dbCom.ExecuteNonQuery();
                     }
 
-                    dbCom = new SQLiteCommand(sqlInsert, dbCon, dbTrans);
-                    dbCom.Parameters.AddWithValue("SourceBatchID", sourceBatch.ID);
-                    dbCom.Parameters.AddWithValue("DestinationBatchID", nDestinationBatchID);
-                    dbCom.Parameters.AddWithValue("DestinationSchemaID", ((naru.db.NamedObject)cboDestination.SelectedItem).ID);
-
                     dbTrans.Commit();
-                    MessageBox.Show("Metrics copy to new schema successful.", CHaMPWorkbench.Properties.Resources.MyApplicationNameLong, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Metric copy to new schema successful.", CHaMPWorkbench.Properties.Resources.MyApplicationNameLong, MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 catch (Exception ex)
                 {
@@ -128,30 +136,58 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
 
         private bool ValidateForm()
         {
-            // Validate the user control of selected batches
-            if (!ucBatch.Validate())
+            if (!(cboProgram.SelectedItem is CHaMPData.Program))
+            {
+                MessageBox.Show("You must choose a program.", "Missing Program", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cboProgram.Select();
                 return false;
+            }
 
             string sourceDBTable = string.Empty;
-            foreach (CHaMPData.MetricBatch batch in ucBatch.SelectedBatches.Values)
+            foreach (DataGridViewRow aRow in grdInfo.Rows)
             {
-                if (string.IsNullOrEmpty(sourceDBTable))
-                    sourceDBTable = batch.DatabaseTable;
-
-                // Check that none of the source metric schemas are selected as the destination schema
-                if (cboDestination.SelectedItem is CHaMPData.MetricSchema)
+                MetricBatch batch = (MetricBatch)aRow.DataBoundItem;
+                if (batch.Copy)
                 {
-                    if (batch.Schema.ID == ((CHaMPData.MetricSchema)cboDestination.SelectedItem).ID)
+                    if (string.IsNullOrEmpty(sourceDBTable))
+                        sourceDBTable = batch.DatabaseTable;
+                    else if (string.Compare(sourceDBTable, batch.DatabaseTable, true) != 0)
                     {
-                        MessageBox.Show("You cannot select one of the source metric schemas as the destination metric schema.", "Invalid Metric Schemas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("You can only select multiple metric schemas that have the same dimensionality. i.e. either all Visit level metric schemas" +
+                            " or all tier 1 etc.", "Invalid Source Metric Schemas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        grdInfo.Select();
                         return false;
                     }
+
+                    // Check that none of the source metric schemas are selected as the destination schema
+                    if (cboDestination.SelectedItem is CHaMPData.MetricSchema)
+                    {
+                        if (batch.Schema.ID == ((CHaMPData.MetricSchema)cboDestination.SelectedItem).ID)
+                        {
+                            MessageBox.Show("You cannot select one of the source metric schemas as the destination metric schema.", "Invalid Metric Schemas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            return false;
+                        }
+                    }
                 }
+            }
+
+            if (string.IsNullOrEmpty(sourceDBTable))
+            {
+                MessageBox.Show("You must select at least one source metric schema.", "Invalid Source Metric Schema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                grdInfo.Select();
+                return false;
             }
 
             if (cboDestination.SelectedIndex < 0)
             {
                 MessageBox.Show("You must select a destination metric schema.", CHaMPWorkbench.Properties.Resources.MyApplicationNameLong, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cboDestination.Select();
+                return false;
+            }
+
+            if (string.Compare(sourceDBTable, ((CHaMPData.MetricSchema)cboDestination.SelectedItem).DatabaseTable, true) != 0)
+            {
+                MessageBox.Show("The source metric schemas must have the same dimensionality as the destination metric schema.", "Invalid Destination Metric Schema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 cboDestination.Select();
                 return false;
             }
@@ -173,9 +209,75 @@ namespace CHaMPWorkbench.Data.Metrics.CopyMetrics
             CHaMPWorkbench.OnlineHelp.FormHelp(this.Name);
         }
 
+        public class MetricBatch : naru.db.NamedObject
+        {
+            public naru.db.NamedObject ScavengeType { get; internal set; }
+            public naru.db.NamedObject Schema { get; internal set; }
+            public naru.db.NamedObject Program { get; internal set; }
+            public string DatabaseTable { get; internal set; }
+            public long Visits { get; internal set; }
+
+            public bool Copy { get; set; }
+
+            public MetricBatch(long nbatchID, long nScavengetTypeID, string sScavengetType, long nSchemaID, string sSchema,
+                long nProgramID, string sProgram, string sDatabaseTable, long? nVisits)
+                : base(nbatchID, string.Format("{0} - {1} - {2}", sProgram, sScavengetType, sSchema))
+            {
+                Schema = new naru.db.NamedObject(nSchemaID, sSchema);
+                ScavengeType = new naru.db.NamedObject(nScavengetTypeID, sScavengetType);
+                Program = new naru.db.NamedObject(nProgramID, sProgram);
+                DatabaseTable = sDatabaseTable;
+                Copy = false;
+
+                Visits = 0;
+                if (nVisits.HasValue)
+                    Visits = nVisits.Value;
+            }
+
+            public static naru.ui.SortableBindingList<MetricBatch> Load()
+            {
+                naru.ui.SortableBindingList<MetricBatch> result = new naru.ui.SortableBindingList<MetricBatch>();
+
+                using (SQLiteConnection dbCon = new SQLiteConnection(naru.db.sqlite.DBCon.ConnectionString))
+                {
+                    dbCon.Open();
+
+                    SQLiteCommand dbCom = new SQLiteCommand("SELECT B.BatchID, B.ScavengeTypeID, L.Title AS ScavengeType, S.ProgramID, P.Title AS Program, S.SchemaID, S.Title AS Schema, S.DatabaseTable, Count(I.BatchID) AS Visits" +
+                        " FROM Metric_Batches B" +
+                            " INNER JOIN Metric_Schemas S ON B.SchemaID = S.SchemaID" +
+                            " INNER JOIN LookupListItems L ON B.ScavengeTypeID = L.ItemID" +
+                            " INNER JOIN LookupPrograms P ON S.ProgramID = P.ProgramID" +
+                            " LEFT JOIN Metric_Instances I ON B.BatchID = I.BatchID" +
+                        " GROUP BY B.BatchID, B.ScavengeTypeID, ScavengeType, S.ProgramID, Program, S.SchemaID, Schema, S.DatabaseTable", dbCon);
+                    SQLiteDataReader dbRead = dbCom.ExecuteReader();
+                    while (dbRead.Read())
+                    {
+                        result.Add(new MetricBatch(dbRead.GetInt64(dbRead.GetOrdinal("BatchID"))
+                            , dbRead.GetInt64(dbRead.GetOrdinal("ScavengeTypeID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("ScavengeType"))
+                            , dbRead.GetInt64(dbRead.GetOrdinal("SchemaID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("Schema"))
+                            , dbRead.GetInt64(dbRead.GetOrdinal("ProgramID"))
+                            , dbRead.GetString(dbRead.GetOrdinal("Program"))
+                            , dbRead.GetString(dbRead.GetOrdinal("DatabaseTable"))
+                            , naru.db.sqlite.SQLiteHelpers.GetSafeValueNInt(ref dbRead, "Visits")));
+                    }
+                }
+
+                return result;
+            }
+        }
+
         private void cboProgram_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cboDestination.DataSource = MetricSchemas.Where<CHaMPData.MetricSchema>(x => x.ProgramID == ucBatch.SelectedProgram.ID).ToList<CHaMPData.MetricSchema>();
+            grdInfo.DataSource = null;
+            cboDestination.DataSource = null;
+
+            if (!(cboProgram.SelectedItem is CHaMPData.Program))
+                return;
+
+            grdInfo.DataSource = new naru.ui.SortableBindingList<MetricBatch>(MetricBatches.Where<MetricBatch>(x => x.Program.ID == ((CHaMPData.Program)cboProgram.SelectedItem).ID).ToList<MetricBatch>());
+            cboDestination.DataSource = MetricSchemas.Where<CHaMPData.MetricSchema>(x => x.ProgramID == ((CHaMPData.Program)cboProgram.SelectedItem).ID).ToList<CHaMPData.MetricSchema>();
         }
     }
 }
